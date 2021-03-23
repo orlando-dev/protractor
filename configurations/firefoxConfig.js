5f04d8477213ba135c4c8ff572ffba2becf6e8fb
Orlando <orlando.junior07@hotmail.com>
2021-03-23 17:47:30 -0300

    Arquivos de configuração

---
+++ configurations/firefoxConfig.js
@@ -0,0 +1,10 @@
+const configCreator = require('../utils/configCreator');
+
+module.exports.config = configCreator({
+    capabilities: { 
+        browserName: 'firefox',
+        'moz:ferifoxOptions': { 
+            args: ['--headless'] //bastante util para rodar os testes em serv de integracao continua
+        }
+    }
+})
