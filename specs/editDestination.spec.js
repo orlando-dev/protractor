da3109dc003e3ef4b6329a50d0eb34254effc8c0
Orlando <orlando.junior07@hotmail.com>
2021-04-05 15:09:09 -0300

    Testando a submissão de um formulário com sucesso

--- specs/editDestination.spec.js
+++ specs/editDestination.spec.js
@@ -1,5 +1,6 @@
 const helper = require('protractor-helper')
-
+const faker = require('faker')
+const Destination = require('../page-objects/destination')
 const EditDestination = require('../page-objects/editDestination')
 
 describe('Given I\'m at a random edit destination page', () => {
@@ -29,6 +30,35 @@ describe('Given I\'m at a random edit destination page', () => {
         it('Then both input fields are wrapped in a .field_with_erros div', () => {
             helper.waitForElementVisibility(editDestination.form.nameInputWithError)
             helper.waitForElementVisibility(editDestination.form.descriptionInputWithError)
+        })
+    })
+
+    describe('When succesfully submiting for the form with a new name and description', () => {
+        let destinationUrl
+        const randomUuid = faker.random.uuid()
+        const fiveRandomWords = faker.random.words(5)
+        
+        beforeEach(() =>{
+            browser.getCurrentUrl().then(url => {
+                destinationUrl = url.replace('/edit', '')
+            })
+
+            editDestination
+            .form
+            .submitFormAfterClearingAndFillingItWith(randomUuid, fiveRandomWords)
+        })
+        it('Then I\m redirected to the destination view page, and I see the updated info ', () =>{
+            const destination = new Destination()
+
+            helper.waitForUrlToBeEqualToExpectedUrl(destinationUrl)
+            helper.waitForTextToBePresentInElement(
+                destination.self.heading,
+                randomUuid
+            )
+            helper.waitForTextToBePresentInElement(
+                destination.self.paragraph,
+                fiveRandomWords
+            )
         })
     })
 })
